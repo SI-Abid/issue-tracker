c919674b91fc3d7f7aafeb9855ce7de2dc7c2e37
Saiham Islam Abid <saiham@inverseai.com>
2024-01-30 10:16:53 +0600

    build edit issue page

---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import IssueForm from "../../_components/IssueForm";
+import { notFound } from "next/navigation";
+
+const EditIssuePage = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const issue = await prisma?.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue) notFound();
+
+  return <IssueForm issue={issue} />;
+};
+
+export default EditIssuePage;
